54176ff858ac98231e75a154631b4cb9a0274647
Brandon McClure <brandon.mcclure@dhha.org>
2015-10-16 11:46:18 -0600

    Added test console app

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelperFuncs;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger myLogger = new Logger();
+            Logger.WriteToLog("Testing");
+
+            myLogger.AddFilePath("\\Metadata.csv", "MetaData");
+            myLogger.EnumerateAcceptableFilePaths();
+
+            Console.ReadLine();
+            return;
+        }
+    }
+}
