be82f5941a17b615d88142ae3e4ba3b5218d7899
Brandon McClure <brandon.mcclure@dhha.org>
2015-12-22 10:25:25 -0700

    Added a class and methods for parsing out arguments. This method uses a custom object with several dictionary values to store the settings and their values, as well as a mapping dictionary of command line arguments, and their corresponding setting keys

--- HelperLibrary/HelperFuncs.cs
+++ HelperLibrary/HelperFuncs.cs
@@ -23,7 +23,8 @@ namespace HelperFuncs
 {
     public class Logger
     {
-        public static void WriteToLog(string input, string fileName = "\\Log.txt", bool append = true, Boolean TerminateLine = true)
+        //This is a general function that writes to a log file. 
+        public static void WriteToLog(string message, string fileName = "\\Log.txt", bool append = true, Boolean TerminateLine = true)
         {
             try
             {
@@ -44,12 +45,12 @@ namespace HelperFuncs
                 {
 
                     if (TerminateLine == true)
-                        file.WriteLineAsync(input);			//Write string and enter newline
+                        file.WriteLineAsync(message);			//Write string and enter newline
                     else
-                        file.WriteAsync(input);
+                        file.WriteAsync(message);
                 }
 
-                Console.WriteLine(input);				//Write to console as well
+                Console.WriteLine(message);				//Write to console as well
             }
             catch (Exception ex)
             {
@@ -65,6 +66,42 @@ namespace HelperFuncs
 
         }
 
+        public static void WriteToCSV(string input, string fileName = "\\Data.csv", bool append = true, Int16 FormatPreset = 1)
+        {
+
+        }
+
+    }
+
+    //Used to store global settings for my apps
+    public class SettingHandler
+    {
+        public Dictionary<string,string> GlobalStrings = new Dictionary<string,string>();
+        public Dictionary<string,Boolean> GlobalBooleans = new Dictionary<string,bool>();
+        public Dictionary<string,Int32> GlobalInt32 = new Dictionary<string,Int32>();
+    }
+
+    public static class ArgumentParser
+    {
+        public static SettingHandler parseArgs( string[] args, SettingHandler AppSettings, Dictionary<string,string> ArgMap)
+        {
+            SettingHandler output = AppSettings;
+
+            foreach (string arg in args)
+            {
+                foreach (KeyValuePair<string, string> map in ArgMap)
+                {
+                    if (arg.Contains(map.Key))
+                    {
+                        if (output.GlobalStrings.ContainsKey(map.Value))
+                        {
+                            output.GlobalStrings[map.Value] = arg.Remove(0, map.Key.Length);
+                        }
+                    }
+                }  
+            }
+            return output;
+        }
     }
     public static class StringExtensions
     {
@@ -117,6 +154,121 @@ namespace HelperFuncs
             output = Regex.Replace(str, @"[^\d]", "");
 
             return output;
+        }
+
+        /*
+         * These are from http://www.codeproject.com/Articles/31050/String-Extension-Collection-for-C
+         */
+        /// <summary>
+        /// true, if is valid email address
+        /// from http://www.davidhayden.com/blog/dave/
+        /// archive/2006/11/30/ExtensionMethodsCSharp.aspx
+        /// </summary>
+        /// <param name="s">email address to test</param>
+        /// <returns>true, if is valid email address</returns>
+
+        public static bool IsValidEmailAddress(this string s)
+        {
+            return new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$").IsMatch(s);
+        }
+
+        /// <summary>
+        /// Checks if url is valid. 
+        /// from http://www.osix.net/modules/article/?id=586
+        /// and changed to match http://localhost
+        /// 
+        /// complete (not only http) url regex can be found 
+        /// at http://internet.ls-la.net/folklore/url-regexpr.html
+        /// </summary>
+        /// <param name="text"></param>
+
+        /// <returns></returns>
+        public static bool IsValidUrl(this string url)
+        {
+            string strRegex = "^(https?://)"
+        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //user@
+        + @"(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP- 199.194.52.184
+        + "|" // allows either IP or domain
+        + @"([0-9a-z_!~*'()-]+\.)*" // tertiary domain(s)- www.
+        + @"([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]" // second level domain
+        + @"(\.[a-z]{2,6})?)" // first level domain- .com or .museum is optional
+        + "(:[0-9]{1,5})?" // port number- :80
+        + "((/?)|" // a slash isn't required if there is no file name
+        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+            return new Regex(strRegex).IsMatch(url);
+        }
+
+        /// <summary>
+
+        /// Reverse the string
+        /// from http://en.wikipedia.org/wiki/Extension_method
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Reverse(this string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new String(chars);
+        }
+
+        /// <summary>
+
+        /// Reduce string to shorter preview which is optionally ended by some string (...).
+        /// </summary>
+        /// <param name="s">string to reduce</param>
+        /// <param name="count">Length of returned string including endings.</param>
+        /// <param name="endings">optional edings of reduced text</param>
+
+        /// <example>
+        /// string description = "This is very long description of something";
+        /// string preview = description.Reduce(20,"...");
+        /// produce -> "This is very long..."
+        /// </example>
+        /// <returns></returns>
+
+        public static string Reduce(this string s, int count, string endings)
+        {
+            if (count < endings.Length)
+                throw new Exception("Failed to reduce to less then endings length.");
+            int sLength = s.Length;
+            int len = sLength;
+            if (endings != null)
+                len += endings.Length;
+            if (count > sLength)
+                return s; //it's too short to reduce
+            s = s.Substring(0, sLength - len + count);
+            if (endings != null)
+                s += endings;
+            return s;
+        }
+
+        /// <summary>
+        /// Remove accent from strings 
+        /// </summary>
+        /// <example>
+        ///  input:  "Příliš žluťoučký kůň úpěl ďábelské ódy."
+        ///  result: "Prilis zlutoucky kun upel dabelske ody."
+        /// </example>
+        /// <param name="s"></param>
+        /// <remarks>founded at http://stackoverflow.com/questions/249087/
+        /// how-do-i-remove-diacritics-accents-from-a-string-in-net</remarks>
+        /// <returns>string without accents</returns>
+
+        public static string RemoveDiacritics(this string s)
+        {
+            string stFormD = s.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            for (int ich = 0; ich < stFormD.Length; ich++)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(stFormD[ich]);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(stFormD[ich]);
+                }
+            }
+            return (sb.ToString().Normalize(NormalizationForm.FormC));
         }
     }
 
